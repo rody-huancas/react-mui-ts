7b9f4e9334f64f5a81b5126f39e9e046648882c8
Rody Huancas <rodyhuancas.04@gmail.com>
2024-01-25 13:57:42 -0500

    Add: connection api

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Container } from "@mui/material";
 import { HeaderComponent } from "../../components";
+import { characters } from "../../api/characters";
 
 export const HomePage: React.FC<{}> = () => {
+  useEffect(() => {
+    characters
+      .getById({ id: 1 })
+      .then((r) => {
+        console.log(r.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <Container maxWidth="xl">
       <HeaderComponent
