1d3c55539aaeb63853a679d138242dc331238987
Rody Huancas <rodyhuancas.04@gmail.com>
2024-01-25 11:16:33 -0500

    Add: login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,82 @@
-import React from "react";
-import { Button, Container } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+type LoginType = {
+  username: string;
+  password: string;
+};
 
 export const LoginPage: React.FC<{}> = () => {
+  const [loginData, setLoginData] = useState<LoginType>({
+    username: "",
+    password: "",
+  });
+
+  const dataLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    console.log(loginData);
+  };
+
   return (
-    <Container sx={{ mt: 9 }} maxWidth="xl">
-      <Button variant="contained">Login</Button>
+    <Container maxWidth="sm">
+      <Grid
+        container
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid item>
+          <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
+            <Typography variant="h4" sx={{ mt: 1, mb: 1 }}>
+              Iniciar sesión
+            </Typography>
+            <Box component={"form"} onSubmit={handleSubmit}>
+              <TextField
+                margin="normal"
+                name="username"
+                type="text"
+                fullWidth
+                label="Email"
+                onChange={dataLogin}
+                sx={{ mt: 1.5, mb: 1.5 }}
+                required
+              />
+              <TextField
+                margin="normal"
+                name="password"
+                type="password"
+                fullWidth
+                label="Password"
+                onChange={dataLogin}
+                sx={{ mt: 1.5, mb: 1.5 }}
+                required
+              />
+              <Button
+                fullWidth
+                type="submit"
+                variant="contained"
+                sx={{ mt: 1.5, mb: 1.5 }}
+              >
+                Iniciar sesión
+              </Button>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
